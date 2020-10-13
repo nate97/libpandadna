4256189c4e156ddad4a06f206253a00cc05d317d
nathan <nate@keemail.me>
2020-10-13 00:01:42 -0500

    Function on Unix

--- src/run_interrogate.py
+++ src/run_interrogate.py
@@ -18,12 +18,12 @@ def run_command(cmd):
 def interrogate(module):
     print('Interrogating', module)
     cmd = os.path.join(pandadir, 'bin', 'interrogate')
-    cmd += ' -D__inline -DCPPPARSER -DP3_INTERROGATE=1 -D__cplusplus -fnames -string -refcount -assert'
-    cmd += ' -S"%(pandadir)s/include/parser-inc" -S"%(pandadir)s/include" -I"%(pandadir)s/include" -I"%(srcdir)s/base" -I"%(srcdir)s/suit" -I"%(srcdir)s/components"'
+    cmd += ' -D__inline -DCPPPARSER -DP3_INTERROGATE=1 -D__cplusplus=201103L -fnames -string -refcount -assert'
+    cmd += ' -S %(pandadir)s/include%(linux)s/parser-inc -S %(pandadir)s/include%(linux)s -I %(pandadir)s/include%(linux)s -I%(srcdir)s/base -I%(srcdir)s/suit -I%(srcdir)s/components'
     cmd += ' -srcdir "%(srcdir)s/%(module)s" -oc "%(srcdir)s/%(module)s_igate.cxx" -od "%(srcdir)s/%(module)s.in" -python-native -DCPPPARSER -D__STDC__=1'
-    cmd += ' -D__cplusplus -D__inline -D_X86_ -DWIN32_VC -DWIN32 -module libpandadna -library %(module)s -Dvolatile='
+    cmd += ' -D__inline -longlong __int64 -D_X86_ %(win32)s -module libpandadna -library %(module)s -Dvolatile='
 
-    cmd = cmd % {'pandadir': pandadir, 'module': module, 'srcdir': srcdir}
+    cmd = cmd % {'pandadir': pandadir, 'module': module, 'srcdir': srcdir, 'linux': '/panda3d' if 'linux' in sys.platform else '', 'win32': '-DWIN32_VC -DWIN32' if sys.platform == 'win32' else ''}
     files = glob.glob(os.path.join(srcdir, module, '*.h'))
     for file in files:
         cmd += ' %s' % os.path.basename(file)
